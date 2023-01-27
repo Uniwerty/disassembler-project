4d4375a89e8a9f9a8811e5be7ed895010265972a
Uniwerty <ivcdim@yandex.ru>
2023-01-27 20:27:51 +0300

    Clear

--- src/main/kotlin/exception/DisassemblerException.kt
+++
@@ -1,9 +0,0 @@
-package exception
-
-sealed class DisassemblerException(reason: String) : RuntimeException(reason) {
-    class UnexpectedInstructionException(instruction: Int) :
-        DisassemblerException("Unexpected instruction found: ${instruction.toString(2)}")
-
-    class UnexpectedRegisterException(register: Int) :
-        DisassemblerException("Unexpected register found: $register")
-}
--- src/main/kotlin/exception/ElfFileException.kt
+++
@@ -1,6 +0,0 @@
-package exception
-
-sealed class ElfFileException(reason: String) : RuntimeException(reason) {
-    class IllegalFileException(criterion: String) :
-        ElfFileException("Illegal file given, not $criterion")
-}
